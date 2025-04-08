a3e85f053f7887532be46e6278084c300930f31d
jjquirogam25 <jjquirogam@udistrital.edu.co>
2025-04-08 16:54:12 -0500

    anadir clase commit

---
+++ verified-commits/main.py
@@ -0,0 +1,10 @@
+class MyCommit():
+    def __init__(self):
+        _content = ""
+        _date = "none"
+
+    def set_content(self,content):
+        print(f"Commit set to: {content}")
+        self._content = content
+
+MyCommit().set_content("Mi primer commit")
